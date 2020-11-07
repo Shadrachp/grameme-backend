0d094cecc143395d394c80a01aa787a3d889eada
Shadrachp <shadrachgabriel@icloud.com>
2020-11-07 15:59:32 +0800

    Add foreign key to post table and users table

--- config/database.sql
+++ config/database.sql
@@ -1,10 +1,27 @@
 CREATE DATABASE grameme;
 
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE post (
   post_id SERIAL PRIMARY KEY,
+  user_id INT,
   title VARCHAR(60) NOT NULL,
   img_link TEXT NOT NULL,
   upvote INT DEFAULT 0,
   downvote INT DEFAULT 0,
-  created_at TIMESTAMPTZ DEFAULT NOW()
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  CONSTRAINT fk_post_users
+    FOREIGN KEY(user_id)
+      REFERENCES users(id)
+      ON DELETE CASCADE
+);
+
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(200) NOT NULL,
+  email VARCHAR(200) NOT NULL,
+  password VARCHAR(200) NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  UNIQUE(email)
 );
