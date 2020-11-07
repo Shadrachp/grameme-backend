504ee0b3699de57550df0b8345dd1f7e31f66d50
Shadrachp <shadrachgabriel@icloud.com>
2020-11-08 04:51:15 +0800

    Add middleware for cors and express-session

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 const express = require("express");
-const app = express();
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
 const cors = require("cors");
 const pool = require("../config/db");
 
+const passport = require('passport');
+const passportStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const app = express();
+
 //Load routes
 const posts = require('./routes/posts');
+const users = require('./routes/users');
 
 //middleware
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(session({
+  secret: "secret",
+  resave: true,
+  saveUninitialized: true
+}))
 
 //Post Routes
 app.use('/posts', posts);
+app.use('/users', users);
 
 //delete post
 
